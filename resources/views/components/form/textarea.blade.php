40d92b41c6b850da1a5c22ebe4f11e8baf565d25
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-05-11 10:48:22 +0200

    Renamed text area components prop bezeichnung to label

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,7 +1,7 @@
-@props(['name', 'value' => '', 'bezeichnung'])
+@props(['name', 'value' => '', 'label'])
 <div>
     <label for="{{ $name }}"
-        class="block mb-2 text-sm font-medium text-[#181818] dark:text-white">{{ $bezeichnung }}</label>
+        class="block mb-2 text-sm font-medium text-[#181818] dark:text-white">{{ $label }}</label>
     <textarea type="textarea" name="{{ $name }}" id="{{ $name }}"
         class="bg-gray-50 text-black border border-gray-300 p-2 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placehoder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         value="">{{ $value }}</textarea>
