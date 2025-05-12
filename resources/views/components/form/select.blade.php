2c6200fb742883a51a2777fb5cbd8877db354b63
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-05-12 20:52:25 +0200

    fixing comparison issue in select component

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -9,7 +9,7 @@
     <select name="{{ $name }}" id="{{ $name }}"
         class="bg-gray-50 text-black border border-gray-300 p-2 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placehoder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         @foreach ($options as $option)
-            <option value="{{ $option->id }}" @if ($value === $option->id) selected="selected" @endif>
+            <option value="{{ $option->id }}" @if ($value == $option->id) selected="selected" @endif>
                 {{ $option->name }}
             </option>
         @endforeach
