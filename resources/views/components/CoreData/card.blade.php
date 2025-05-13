0ac2f20b7fc9d1d9e9c879de3d368b3fa3316aab
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-05-13 20:32:31 +0200

    Cards to display coredata page

---
+++ resources/views/components/CoreData/card.blade.php
@@ -0,0 +1,12 @@
+@props(['name', 'url' => null])
+<a class="p-4 md:w-1/4 sm:w-1/2 w-full" href="{{ $url }}">
+    <card>
+        <div
+            class="max-w-xl border-2 border-gray-600 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-105">
+
+            <p class="leading-relaxed dark:text-white">
+                {{ $name }}
+            </p>
+        </div>
+    </card>
+</a>
