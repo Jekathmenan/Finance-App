44149ee00d4b29270ce1271d3cc821463a08eb5a
Jekathmenan Selvarajah <jekath27@gmail.com>
2025-05-14 21:27:01 +0200

    Admin panel overview
    Currently there is only usermanagement

---
+++ resources/views/auth/AdminPanel/overview.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.layout')
+@section('head')
+    <link rel="stylesheet" href="/styles/tabs.css">
+@endsection
+@section('body')
+    <section class="text-gray-700 body-font">
+        <div class="m-6 container px-5 py-24 mx-6 ">
+            <div class="m-6 flex flex-wrap -m-4 text-center">
+                {{-- rendering a card to add a new transfertype --}}
+                <x-coredata.card name="Benutzerverwaltung" url="/users/" />
+            </div>
+        </div>
+    </section>
+@endsection
